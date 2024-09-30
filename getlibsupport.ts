901c705bf58555cc1699585ab9e9ddad50f51683
O F <teleclimber@gmail.com>
2024-09-30 12:11:10 -0700

    fix: use globalThis instead of window
    - fixes Deno 2 compatibility

--- getlibsupport.ts
+++ getlibsupport.ts
@@ -1,6 +1,6 @@
 import type libSupportIface from 'https://deno.land/x/dropserver_lib_support@v0.2.0/mod.ts';
 
-const w = <{["DROPSERVER"]?:libSupportIface}>window;
+const w = <{["DROPSERVER"]?:libSupportIface}>globalThis;
 export default function mustGetLibSupport() :libSupportIface {
 	if( w["DROPSERVER"] === undefined ) throw new Error("Dropserver instance not present.");
 	return w["DROPSERVER"];
